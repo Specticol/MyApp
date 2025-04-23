3f13a905ebc1bb5d5e1102daa5ed416ca7805911
Specticol <vincent.vansteenhuyse10@gmail.com>
2025-04-23 20:27:38 +0200

    bongo

--- PersonalApp/src/pages/Mhw/Armor.tsx
+++ PersonalApp/src/pages/Mhw/Armor.tsx
@@ -6,13 +6,13 @@ type SlotProps = {
     Slots: number[]
 }
 
-
+// Fetches APi
 async function fetchArmor(): Promise<Armor[]> {
     const response = await fetch('https://wilds.mhdb.io/en/armor');
     const data = await response.json();
     return data;
 }
-
+// Shows Slots nicely
 function SlotAmount({ Slots }: SlotProps) {
     let SlotShown: string[] = [];
     for (let i = 0; i < 3; i++) {
@@ -24,11 +24,11 @@ function SlotAmount({ Slots }: SlotProps) {
             SlotShown.push(`\u2462`)
         } else { SlotShown.push(`\u3280`) }
     }
-    return (
-        <>
-            <p>Slots: {SlotShown.map(a => (a))}</p>
-        </>
-    )
+    return <p>Slots: {SlotShown.map(a => (a))}</p>
+}
+//group armor sets into array
+function groupArmorSets(){
+    
 }
 
 
@@ -37,8 +37,10 @@ export default function MhwArmor() {
     const [armor, setArmor] = useState<Armor[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState("");
+    const [selectedOption, setSelectedOption] = useState("infoPieces");
 
 
+    // Loadin Api
     useEffect(() => {
         setIsLoading(true);
         fetchArmor()
@@ -49,7 +51,7 @@ export default function MhwArmor() {
                 setIsLoading(false);
             });
     }, []);
-
+    // Searchbar
     const filteredArmor = armor.filter((a) => {
         const nameMatch = a.name.toLowerCase().includes(searchTerm.toLowerCase());
         const skillMatch = a.skills.some(skill =>
@@ -71,6 +73,7 @@ export default function MhwArmor() {
     return (
         <>
             <h2>Armor Page WIP</h2>
+            {/*SearchBar */}
             <div>
                 <input
                     type="text"
@@ -79,28 +82,69 @@ export default function MhwArmor() {
                     onChange={(e) => setSearchTerm(e.target.value)}
                     style={{ padding: "0.5rem", margin: "1rem 0", width: "100%" }}
                 />
-
             </div>
+            {/*RadioCheck */}
             <div>
+                <label>
+                    <input
+                        type="radio"
+                        name="info"
+                        value="infoPieces"
+                        checked={selectedOption === "infoPieces"}
+                        onChange={(e) => setSelectedOption(e.target.value)}
+                    />
+                    Armor Pieces
+                </label>
 
-                {
-                    filteredArmor.map((a, i) => (
-                        <div key={i} style={{ backgroundColor: "#1e1e1e" }}>
-
-                            <p>{a.kind} piece</p>
-                            <p>Name: {a.name}</p>
-                            <p>Rank: {a.rank}</p>
-                            <p>Defense: {a.defense.base}</p>
-                            <div>{a.skills.map(skill => (
-                                <p key={skill.skill.id}>Skill: {skill.skill.name}</p>
-                            ))}</div>
-
-
-                            <p>{<SlotAmount Slots={a.slots} />}</p>
-                        </div>
-                    ))
-                }
+                <label>
+                    <input
+                        type="radio"
+                        name="info"
+                        value="infoSets"
+                        checked={selectedOption === "infoSets"}
+                        onChange={(e) => setSelectedOption(e.target.value)}
+                    />
+                    Armor Sets
+                </label>
             </div>
+            {/*Armor pieces */}
+            {selectedOption === "infoPieces" && (
+                <div>
+                    {
+                        filteredArmor.map((a, i) => (
+                            <div key={i} style={{ backgroundColor: `var(--bg-color)` }}>
+
+                                <p>{a.kind} piece</p>
+                                <p>Name: {a.name}</p>
+                                <p>Rank: {a.rank}</p>
+                                <p>Defense: {a.defense.base}</p>
+                                <div>{a.skills.map(skill => (
+                                    <p key={skill.skill.id}>Skill: {skill.skill.name}</p>
+                                ))}</div>
+
+                                <p>{<SlotAmount Slots={a.slots} />}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            )}
+            {/*Armor set */}
+
+            {selectedOption === "infoSets" && (
+                <div>
+                    {
+                        filteredArmor.map((a, i) => (
+                            <div key={i} style={{ backgroundColor: `var(--bg-color)` }}>
+
+                                <p>{a.armorSet.name} set</p>
+
+                            </div>
+                        ))
+                    }
+                </div>
+            )}
+
+
         </>
     )
 }
